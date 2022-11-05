487fecdcbca40d72ce14505213ad6bbdfa4c7bc6
Hambarzum <hambarcummuradyan4@gmail.com>
2022-11-05 15:33:25 +0400

    new updates

--- src/game/game.hpp
+++ src/game/game.hpp
@@ -3,17 +3,24 @@
 #include <vector>
 
 #include "board.hpp"
+#include "game_object.hpp"
 #include "menu.hpp"
+#include "snake.hpp"
+
 
 using GameObjects = std::vector<GameObject>;
 
 class Game {
 public:
-    Game(Menu mainMenu, Board board, Snake snake);
+    Game(Menu mainMenu, Board& board, Snake& snake);
+
+public:
+    bool isOver();
+    void 
 
 private:
     Menu mainMenu_;
     Board board_;
-    Snake snake_;
+    Snake player_;
     GameObject fruit_;
 };
